ba2532e8f3f2a033e13cc3559eda6e9544ffbaa1
munificent-cast-9719 <agomezdemen15@gmail.com>
2020-03-03 18:55:41 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,13 +1,12 @@
 def find_element_index(array, value_to_find)
-  bool = true
   counter = 1
   
   while counter < array.length do
     if array[counter] == value_to_find
       puts counter
+    else
+      puts nil
   end
-  
-  
 end
 
 def find_max_value(array)
